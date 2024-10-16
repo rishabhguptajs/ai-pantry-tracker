ce6e097e6edce1dacd79f4dea72549c7b4989a31
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-10-16 12:31:38 +0530

    fix: update pantry item handling and improve recipe suggestion logic
    - Enhanced the recipe suggestion feature to handle errors more gracefully.
    - Improved the image classification process for pantry items.
    - Added loading states for better user experience during item addition and recipe suggestion.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,82 @@
-"use client";
+"use client"
 
-import React from 'react';
-import Link from 'next/link';
-import { useAuth } from '@/context/AuthContext';
+import React from "react"
+import Link from "next/link"
+import { useAuth } from "@/context/AuthContext"
+import Image from "next/image"
 
 export default function Home() {
-  const { user } = useAuth();
+  const { user } = useAuth()
 
   return (
-    <main className="flex flex-col items-center justify-center p-24 min-h-screen bg-gray-100">
-      <header className="text-center mb-12">
-        <h1 className="text-4xl font-bold text-blue-600 mb-4">Welcome to Your Pantry Manager!</h1>
-        <p className="text-lg text-gray-700">Manage your pantry and discover new recipes effortlessly.</p>
-      </header>
-      
-      <section className="text-center mb-12">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-6">Get Started</h2>
-        <p className="text-base text-gray-600 max-w-md mx-auto mb-6">
-          Easily add, update, and manage the items in your pantry. Let our AI suggest delicious recipes based on what you have!
-        </p>
-        
-        { user ? (
-          <Link href="/dashboard"  className="bg-blue-600 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-700">
-            Go to Pantry
-          </Link>
-        ) : (
-          <Link href="/login"  className="bg-blue-600 text-white px-6 py-3 rounded-md shadow-md hover:bg-blue-700">
-            Sign In
-          </Link>
-        ) }
+    <main className="flex flex-col items-center justify-center p-12 min-h-screen bg-white">
+      <section className="text-left mb-12 flex items-center flex-row w-full">
+        <div className="w-1/2">
+          <h1 className="text-4xl font-bold text-green-500 mb-4">
+            Welcome to Shelfie
+          </h1>
+          <p className="text-lg text-gray-700 font-normal">
+            Manage your{" "}
+            <span className="italic font-bold text-green-600">
+              favorite foods
+            </span>{" "}
+            and get customized{" "}
+            <span className="italic font-bold text-pink-600/90">
+              AI recipe suggestions
+            </span>
+            , anytime, anywhere!
+          </p>
 
+          <div className="mt-8 text-left">
+            {user ? (
+              <Link
+                href="/dashboard"
+                className="bg-amber-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-amber-600/9"
+              >
+                Go to Pantry
+              </Link>
+            ) : (
+              <Link
+                href="/login"
+                className="bg-amber-500 text-white px-6 py-3 rounded-md shadow-md hover:bg-amber-600/85"
+              >
+                Sign In
+              </Link>
+            )}
+          </div>
+        </div>
+        <div className="w-1/2 flex flex-col gap-3">
+          <Image
+            src="/foodimg.png"
+            alt="Pantry"
+            width={500}
+            height={500}
+            className="rounded-2xl"
+            layout="responsive"
+          />
+          <div className="w-full flex flex-row space-x-2 items-center justify-center">
+            <span className="bg-pink-500 w-fit p-2 rounded-3xl text-white text-xs text-center font-jacaurdaFont font-bold uppercase cursor-default">
+              add some pasta, huh?
+            </span>
+            <span className="bg-green-500 w-fit p-2 rounded-3xl text-white text-xs text-center font-jacaurdaFont font-bold uppercase cursor-default">
+              maybe some cheese?
+            </span>
+            <span className="bg-pink-500 w-fit p-2 rounded-3xl text-white text-xs text-center font-jacaurdaFont font-bold uppercase cursor-default">
+              or some veggies?
+            </span>
+            <span className="bg-green-500 w-fit p-2 rounded-3xl text-white text-xs text-center font-jacaurdaFont font-bold uppercase cursor-default">
+              or more.
+            </span>
+          </div>
+        </div>
       </section>
-      
+
+      <section className="text-center mb-12"></section>
+
       <section className="text-center">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-6">Why Use Our App?</h2>
+        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+          Why Use Our App?
+        </h2>
         <ul className="text-base text-gray-600 max-w-md mx-auto list-disc list-inside">
           <li className="mb-2">Keep track of your pantry items</li>
           <li className="mb-2">Get recipe suggestions based on your pantry</li>
@@ -42,10 +85,12 @@ export default function Home() {
           <li className="mb-2">Classification of images using AI</li>
         </ul>
       </section>
-      
+
       <footer className="text-center mt-12">
-        <p className="text-sm text-gray-500">&copy; 2024 Pantry Manager. All rights reserved.</p>
+        <p className="text-sm text-gray-500">
+          &copy; 2024 Pantry Manager. All rights reserved.
+        </p>
       </footer>
     </main>
-  );
+  )
 }
