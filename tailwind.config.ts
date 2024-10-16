4b696aecd491c06dac4654b3eb2f6163418d6dd5
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-10-16 12:31:20 +0530

    Add functionality to suggest recipes based on user ingredients and improve item handling in the pantry.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        headingFont: ["Archivo", "system-ui"],
+        jacaurdaFont: ["Bebas Neue", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
